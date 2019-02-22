d774b419a7a8f6069b9a1b69fd0fb5c25719fcbe
JesusGuerrero <rocha.jesus@hotmail.com>
2019-02-22 01:42:00 -0800

    plugin calls native alert after 7 seconds

--- Assets/Plugins/iOS/MyPlugin.mm
+++ Assets/Plugins/iOS/MyPlugin.mm
@@ -1,7 +1,11 @@
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface MyPlugin: NSObject
+typedef void (*INT_CALLBACK)(int);
+
+@interface MyPlugin: NSObject <UIAlertViewDelegate>
 {
+    INT_CALLBACK alertCallBack;
     NSDate *creationDate;
 }
 @end
@@ -18,6 +22,13 @@ static MyPlugin *_sharedInstance;
         _sharedInstance = [[MyPlugin alloc] init];
     });
     return _sharedInstance;
+}
+
++(NSString*)createNSString:(const char*) string {
+    if (string!=nil)
+        return [NSString stringWithUTF8String:string];
+    else
+        return @"";
 }
 
 -(id)init
@@ -39,6 +50,36 @@ static MyPlugin *_sharedInstance;
     return [[NSDate date] timeIntervalSinceDate:creationDate];
 }
 
+-(void)createAlertDialog:(const char**) strings_in stringCount:(int)stringCount callback:(INT_CALLBACK)callback
+{
+    NSMutableArray *strings = [NSMutableArray new];
+    for (int ix=0;ix<stringCount;ix++)
+    {
+        if (strlen(strings_in[ix])>0)
+            [strings addObject:[MyPlugin createNSString:strings_in[ix]]];
+    }
+    stringCount = (int)[strings count];
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:strings[0] message:strings[1] delegate:self cancelButtonTitle:strings[2] otherButtonTitles:nil];
+    if (stringCount>3)
+    {
+        int ix = 3;
+        while (ix<stringCount)
+        {
+            [alertView addButtonWithTitle:strings[ix]];
+            ix++;
+        }
+    }
+    alertCallBack = callback;
+    
+    [alertView show];
+}
+
+-(void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"iOS: clicked button index: %ld",buttonIndex);
+    alertCallBack((int)buttonIndex);
+}
 @end
 
 extern "C"
@@ -47,4 +88,10 @@ extern "C"
     {
         return [[MyPlugin sharedInstance] getElapsedTime];
     }
+    
+    void IOScreateNativeAlert(const char** strings, int stringCount, INT_CALLBACK callback)
+    {
+        [[MyPlugin sharedInstance] createAlertDialog:strings stringCount:stringCount callback:callback];
+    }
+    
 }
